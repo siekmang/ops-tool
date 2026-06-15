66bf291495bc7aa43a9090d32dd2a19784b1480a
Greg Siekman <github@siekmang.com>
2026-06-15 15:20:24 -0400

    Getting UI in place

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,156 +1,90 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
+  import { Command } from '@tauri-apps/plugin-shell'
 
-  let name = $state("");
-  let greetMsg = $state("");
+  function testLxdTool() {
 
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
   }
+
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
+  <!--Want better fonts-->
+  <!--Style better, especially buttons-->
+  <!--Need to set default text editor/ide-->
+  <h1>Greg's Ops Tool</h1>
 
-  <div class="row">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
+  <div id='tools-menus'>
+    <div id='lxd-tools'>
+      <h2>LXD Tools</h2>
+      <!--Close any other windows to open this-->
+      <button>Open lxd-tools</button>
+      <button>Test lxd-tools</button>
+      <button>Build lxd-tools locally</button>
+      <button>lxd-tools git status</button>
+      <button>Open lxd-tools in GitHub</button>
+    </div>
+
+    <div id='misc'>
+      <h2>Misc</h2>
+      <!--Want this to evolve to let the user do text input, browse input or recent dropdown -->
+      <input type="text" id="path" placeholder="Repo path"/>
+      <button>Open Repo</button>
+      <button>Repo git status</button>
+    </div>
   </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
 
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
+
 </main>
 
 <style>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.svelte-kit:hover {
-  filter: drop-shadow(0 0 2em #ff3e00);
-}
-
-:root {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 400;
-
-  color: #0f0f0f;
-  background-color: #f6f6f6;
-
-  font-synthesis: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-text-size-adjust: 100%;
-}
-
-.container {
-  margin: 0;
-  padding-top: 10vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: 0.75s;
-}
-
-.logo.tauri:hover {
-  filter: drop-shadow(0 0 2em #24c8db);
-}
-
-.row {
-  display: flex;
-  justify-content: center;
-}
-
-a {
-  font-weight: 500;
-  color: #646cff;
-  text-decoration: inherit;
-}
-
-a:hover {
-  color: #535bf2;
-}
-
-h1 {
-  text-align: center;
-}
-
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
-button {
-  cursor: pointer;
-}
-
-button:hover {
-  border-color: #396cd8;
-}
-button:active {
-  border-color: #396cd8;
-  background-color: #e8e8e8;
-}
-
-input,
-button {
-  outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
-}
-
-@media (prefers-color-scheme: dark) {
   :root {
-    color: #f6f6f6;
-    background-color: #2f2f2f;
+    background-color:#1d2433;
+    color: #d7dce2;
   }
 
-  a:hover {
-    color: #24c8db;
+  h1 {
+    text-align: center;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
   }
 
-  input,
+  h2 {
+    text-align: center;
+  }
+
   button {
-    color: #ffffff;
-    background-color: #0f0f0f98;
+    background-color: #6679a4;
+    color: #d8dce2;
+    border-radius: 5px;
+    margin: .1vh 0;
+    padding: 2vh 0;
   }
-  button:active {
-    background-color: #0f0f0f69;
+
+  button:hover {
+    background-color: #96a6cc;
   }
-}
+
+  #tools-menus {
+    margin: 0% 5%;
+    display: flex;
+    gap: 2rem;
+  }
+
+  #lxd-tools, #misc {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  input {
+    background-color: #d7dce2;
+    color: #171c28;
+    padding: 5px 0;
+    padding-left: 5px;
+  }
+
+  ::placeholder {
+    color: #a2aabc;
+  }
 
 </style>
